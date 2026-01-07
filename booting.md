11139ceb93d624d7ff3d84c975f0a65a71f6c940
CARBANOID <sushant2004negi@gmail.com>
2026-01-07 15:12:55 +0530

    01: shadcn added

---
+++ booting.md
@@ -0,0 +1,3 @@
+npx create-next-app@15.5.4
+npx shadcn@3.3.1 init
+npx shadcn@3.3.1 add --all
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home(){
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+export default function Home1(){
+  const temp : boolean = true ; 
   return (
-    <div>
-      <h1 className="text-amber-700">Welcome to the Home Page</h1>
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      <Button variant="outline">Click Me</Button>
+    </div>
+  )
+}
+
+export  function Home2(){
+  const temp : boolean = true ; 
+  return (
+    <div className={cn("text-amber-700 font-extrabold",temp && "text-green-500")}>
+      Welcome to the Home Page
     </div>
   )
 }

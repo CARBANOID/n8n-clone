0d0caf77f12374e0585343b2028bb69204ae30b1
CARBANOID <sushant2004negi@gmail.com>
2026-01-27 23:33:37 +0530

    15 : editor (react-flow)

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -1,7 +1,12 @@
 "use client"
 import { ErrorView, LoadingView } from "@/components/entity-components"
 import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows"
-
+import { useState , useCallback } from "react"
+import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge,Background, Controls, MiniMap , Panel } from '@xyflow/react';
+import type { Node, Edge, NodeChange, EdgeChange, Connection } from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+import { nodeComponents } from "@/config/node-components";
+import { AddNodeButton } from "@/components/add-node-button";
 
 export const EditorLoading = () => {
    return <LoadingView message="Loading editor..."/>
@@ -11,13 +16,48 @@ export const EditorError = () => {
     return <ErrorView message="Error in loading editor!!"/>
 }
 
-
 export const Editor = ( { workflowId } : { workflowId : string }) => {
     const { data : workflow } = useSuspenseWorkflow(workflowId) ; 
 
+    const [nodes, setNodes] = useState<Node[]>(workflow.nodes);
+    const [edges, setEdges] = useState<Edge[]>(workflow.edges);
+    
+    const onNodesChange = useCallback(
+        (changes : NodeChange[]) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+        [],
+    );
+    const onEdgesChange = useCallback(
+        (changes: EdgeChange[]) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+        [],
+    );
+    const onConnect = useCallback(
+        (params : Connection) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+        [],
+    );
+ 
     return (
-        <p>
-            { JSON.stringify(workflow,null,2)}
-        </p>
+    <div className="size-full">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+        nodeTypes={nodeComponents}
+        proOptions={{
+            hideAttribution : true,  // to hide the reactflow logo at left bottom
+        }}
+      > 
+        <Background/> {/* To add grids in the background */}
+        <Controls/>   {/* To add zoom in and out with lock functionality*/}
+        <MiniMap/>   {/* To create a mini map of the flow */}
+      
+        <Panel position="top-right">
+            {/* To add an custom panel to react flow canvas */}
+            <AddNodeButton/>
+        </Panel>
+      </ReactFlow>  
+    </div>
     )
 }
